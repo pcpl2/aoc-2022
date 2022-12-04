8461785eb1c89e45167ad5629744dda5dc5736a6
Patryk 'pcpl2' Ławicki <kontakt@patryk-lawicki.pl>
2022-12-04 07:51:11 +0100

    Added day4 part 2

--- day4/main.go
+++ day4/main.go
@@ -9,9 +9,15 @@ import (
 	"strings"
 )
 
+type ElfData struct {
+	Start int
+	End   int
+	Range []int
+}
+
 type ElfPair struct {
-	ElfFirst  []int
-	ElfSecond []int
+	ElfFirst  ElfData
+	ElfSecond ElfData
 }
 
 func main() {
@@ -21,18 +27,23 @@ func main() {
 	}
 	defer file.Close()
 
-	var totalPairs = 0
+	var totalPairsPart1 = 0
+	var totalPairsPart2 = 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		data := parsePairData(scanner.Text())
 		pd := checkOneReangeFyllyContainOther(data)
 		if pd {
-			totalPairs = totalPairs + 1
+			totalPairsPart1 = totalPairsPart1 + 1
 		}
-		log.Printf("Data :%v | %v", data, pd)
+		pd2 := checkPairRangeOverlap(data)
+		if pd2 {
+			totalPairsPart2 = totalPairsPart2 + 1
+		}
 	}
 
-	log.Printf("Pairs have range fully contain the orher: %d", totalPairs)
+	log.Printf("Pairs have range fully contain the other: %d", totalPairsPart1)
+	log.Printf("Pairs have range overlap: %d", totalPairsPart2)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -41,19 +52,30 @@ func main() {
 
 func parsePairData(input string) ElfPair {
 	elfs := strings.Split(input, ",")
-	firstElf := make([]int, 0)
-	secondElf := make([]int, 0)
+	firstElf := ElfData{
+		Range: make([]int, 0),
+	}
+	secondElf := ElfData{
+		Range: make([]int, 0),
+	}
 
 	for elfi, elf := range elfs {
 		elfRange := strings.Split(elf, "-")
 		start, _ := strconv.Atoi(elfRange[0])
 		end, _ := strconv.Atoi(elfRange[1])
+		if elfi == 0 {
+			firstElf.Start = start
+			firstElf.End = end
+		} else if elfi == 1 {
+			secondElf.Start = start
+			secondElf.End = end
+		}
 
 		for i := start; i <= end; i++ {
 			if elfi == 0 {
-				firstElf = append(firstElf, i)
+				firstElf.Range = append(firstElf.Range, i)
 			} else if elfi == 1 {
-				secondElf = append(secondElf, i)
+				secondElf.Range = append(secondElf.Range, i)
 			}
 		}
 	}
@@ -65,17 +87,22 @@ func parsePairData(input string) ElfPair {
 }
 
 func checkOneReangeFyllyContainOther(pair ElfPair) bool {
-	totalRange := append(pair.ElfFirst, pair.ElfSecond...)
+	totalRange := append(pair.ElfFirst.Range, pair.ElfSecond.Range...)
 	totalRange = removeDuplacated(totalRange)
 
 	sort.SliceStable(totalRange, func(i, j int) bool {
 		return i < j
 	})
 
-	if len(totalRange) == len(pair.ElfFirst) || len(totalRange) == len(pair.ElfSecond) {
+	if len(totalRange) == len(pair.ElfFirst.Range) || len(totalRange) == len(pair.ElfSecond.Range) {
 		return true
 	}
 	return false
+}
+
+func checkPairRangeOverlap(pair ElfPair) bool {
+	log.Printf("%v | %d | %d", pair, pair.ElfSecond.Start, pair.ElfFirst.End)
+	return !(pair.ElfFirst.End < pair.ElfSecond.Start || pair.ElfFirst.Start > pair.ElfSecond.End)
 }
 
 func removeDuplacated(elfTotalRange []int) []int {
--- day4/main_test.go
+++ day4/main_test.go
@@ -2,58 +2,127 @@ package main
 
 import "testing"
 
+var td01 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 2,
+		End:   4,
+		Range: []int{2, 3, 4},
+	},
+	ElfSecond: ElfData{
+		Start: 6,
+		End:   8,
+		Range: []int{6, 7, 8},
+	},
+}
+var td02 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 2,
+		End:   3,
+		Range: []int{2, 3},
+	},
+	ElfSecond: ElfData{
+		Start: 4,
+		End:   5,
+		Range: []int{4, 5},
+	},
+}
+var td03 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 5,
+		End:   7,
+		Range: []int{5, 6, 7},
+	},
+	ElfSecond: ElfData{
+		Start: 7,
+		End:   9,
+		Range: []int{7, 8, 9},
+	},
+}
+var td04 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 2,
+		End:   8,
+		Range: []int{2, 3, 4, 5, 6, 7, 8},
+	},
+	ElfSecond: ElfData{
+		Start: 3,
+		End:   7,
+		Range: []int{3, 4, 5, 6, 7},
+	},
+}
+var td05 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 6,
+		End:   6,
+		Range: []int{6},
+	},
+	ElfSecond: ElfData{
+		Start: 4,
+		End:   6,
+		Range: []int{4, 5, 6},
+	},
+}
+var td06 = ElfPair{
+	ElfFirst: ElfData{
+		Start: 2,
+		End:   6,
+		Range: []int{2, 3, 4, 5, 6},
+	},
+	ElfSecond: ElfData{
+		Start: 4,
+		End:   8,
+		Range: []int{4, 5, 6, 7, 8},
+	},
+}
+
 func TestResultPart1(t *testing.T) {
-	result := checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{2, 3, 4},
-		ElfSecond: []int{6, 7, 8},
-	})
-
-	if result {
+	if checkOneReangeFyllyContainOther(td01) {
 		t.FailNow()
 	}
 
-	result = checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{2, 3},
-		ElfSecond: []int{4, 5},
-	})
-
-	if result {
+	if checkOneReangeFyllyContainOther(td02) {
 		t.FailNow()
 	}
 
-	result = checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{5, 6, 7},
-		ElfSecond: []int{7, 8, 9},
-	})
-
-	if result {
+	if checkOneReangeFyllyContainOther(td03) {
 		t.FailNow()
 	}
 
-	result = checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{2, 3, 4, 5, 6, 7, 8},
-		ElfSecond: []int{3, 4, 5, 6, 7},
-	})
-
-	if !result {
+	if !checkOneReangeFyllyContainOther(td04) {
 		t.FailNow()
 	}
 
-	result = checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{6},
-		ElfSecond: []int{4, 5, 6},
-	})
-
-	if !result {
+	if !checkOneReangeFyllyContainOther(td05) {
 		t.FailNow()
 	}
 
-	result = checkOneReangeFyllyContainOther(ElfPair{
-		ElfFirst:  []int{2, 3, 4, 5, 6},
-		ElfSecond: []int{4, 5, 6, 7, 8},
-	})
+	if checkOneReangeFyllyContainOther(td06) {
+		t.FailNow()
+	}
+}
 
-	if result {
+func TestResultPart2(t *testing.T) {
+	if checkPairRangeOverlap(td01) {
+		t.FailNow()
+	}
+
+	if checkPairRangeOverlap(td02) {
+		t.FailNow()
+	}
+
+	if !checkPairRangeOverlap(td03) {
+		t.FailNow()
+	}
+
+	if !checkPairRangeOverlap(td04) {
+		t.FailNow()
+	}
+
+	if !checkPairRangeOverlap(td05) {
+		t.FailNow()
+	}
+
+	if !checkPairRangeOverlap(td06) {
 		t.FailNow()
 	}
 }
