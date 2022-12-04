6ff63f0f3b9a0e1547cf531dda28e28516c4524f
Patryk 'pcpl2' Ławicki <kontakt@patryk-lawicki.pl>
2022-12-04 01:44:41 +0100

    Added day1 part two

--- day1/main.go
+++ day1/main.go
@@ -9,6 +9,7 @@ import (
 
 type Elf struct {
 	TotalCalories int
+	ElfNumber     int
 }
 
 func main() {
@@ -24,7 +25,7 @@ func main() {
 
 	//Init list
 	elfs = make([]Elf, 0)
-	elfs = append(elfs, Elf{TotalCalories: 0})
+	elfs = append(elfs, Elf{TotalCalories: 0, ElfNumber: 1})
 
 	var currentElf = len(elfs) - 1
 	for scanner.Scan() {
@@ -36,7 +37,7 @@ func main() {
 			}
 		} else {
 			log.Println("New elf")
-			elfs = append(elfs, Elf{TotalCalories: 0})
+			elfs = append(elfs, Elf{TotalCalories: 0, ElfNumber: currentElf + 1})
 			currentElf = len(elfs) - 1
 		}
 	}
@@ -46,12 +47,14 @@ func main() {
 	}
 
 	elfs = sortElfs(elfs)
-	log.Printf("Elf carrying the most Calories: %d", elfs[0].TotalCalories)
+	log.Printf("Total number of calories carried by the elf having the most calories: %d", elfs[0].TotalCalories)
+	totalOfTopThree := elfs[0].TotalCalories + elfs[1].TotalCalories + elfs[2].TotalCalories
+	log.Printf("Total calories carried by the 3 elves with the most calories: %d", totalOfTopThree)
 }
 
 func sortElfs(elfs []Elf) []Elf {
 	for i := 0; i < len(elfs)-1; i++ {
-		for j := 0; j < len(elfs)-j-1; j++ {
+		for j := 0; j < len(elfs)-i-1; j++ {
 			if elfs[j].TotalCalories < elfs[j+1].TotalCalories {
 				elfs[j], elfs[j+1] = elfs[j+1], elfs[j]
 			}
